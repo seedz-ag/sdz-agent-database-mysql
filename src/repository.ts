66b0570e79c99671bb4d0b106ef2b0a3a10d25d7
Rubens Curvello <rubens.curvello@rhimto.com.br>
2021-11-01 11:02:25 -0300

    limit and offset mysql

--- src/repository.ts
+++ src/repository.ts
@@ -1,4 +1,14 @@
 import { AbstractRepository } from "sdz-agent-types";
 
 export default class OracleRepository extends AbstractRepository {
+    execute(query: string, page?: number, limit?: number): Promise<any> {
+        const statement = [
+          query,
+          limit ? `LIMIT ${limit}` : null,
+          page && limit ? `OFFSET ${page * limit}` : null,
+        ]
+          .filter((item) => !!item)
+          .join(" ");
+        return this.getConnector().execute(statement);
+      }
 }
